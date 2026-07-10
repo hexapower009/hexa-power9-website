e142794d61e4a3c82a5758049e12cb04cb4c2d0a
Hexa Power 9 <hexapower009@gmail.com>
2026-07-10 20:20:41 +0300

    Update website domain

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-const siteUrl = "https://hexa-power9.vercel.app";
+const siteUrl = "https://hexapoweralnakheel.com";
 
 export default function robots(): MetadataRoute.Robots {
   return {
